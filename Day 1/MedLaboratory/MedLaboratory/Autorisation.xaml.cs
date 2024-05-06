79b26459fbd27146b77fe4841b24cd0cb619cc13
ALTERRA4546 <kordroy20@mail.ru>
2024-05-06 16:50:53 +0300

    Day 1

---
+++ Day 1/MedLaboratory/MedLaboratory/Autorisation.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MedLaboratory
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorisation.xaml
+    /// </summary>
+    public partial class Autorisation : Window
+    {
+        public Autorisation()
+        {
+            InitializeComponent();
+
+            passwordV.Visibility = Visibility.Collapsed;
+        }
+
+        public bool passwordVisability = false;
+
+        public static class userData
+        { 
+            public static int idUser {  get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (login.Text == "" || passwordH.Password == "")
+            {
+                MessageBox.Show("Заполните поля");
+                return;
+            }
+
+            using (var bd = new MedLaboratoryEntities())
+            {
+                string log = login.Text;
+                string pas = passwordH.Password;
+                var user = bd.Авторизация.FirstOrDefault(p=>p.Логин == log && p.Пароль == pas);
+                if (user != null)
+                {
+                    if ((user.Время_блокировки != null) && (Convert.ToInt32((DateTime.Now - user.Время_блокировки).Value.TotalMinutes) <= 30))
+                    {
+                        MessageBox.Show($"Учетная запись временно заблокированна. До конца блокировки {30 - ((DateTime.Now - user.Время_блокировки).Value.Minutes)} минут.");
+                        return;
+                    }
+
+                    var role = from pol in bd.Пользователи
+                               join
+                               rol in bd.Должность on pol.Код_должности equals rol.Код_должности
+                               where (pol.Код_пользователя == user.Код_пользователя)
+                               select rol.Название;
+                    switch (role.FirstOrDefault())
+                    {
+                        case "лаборант":
+                            UpdateTime(Convert.ToInt32(user.Код_пользователя));
+                            LaboratoryAssistant la = new LaboratoryAssistant();
+                            la.Show();
+                            this.Hide();
+                            break;
+                        
+                        case "лаборант-исследователь":
+                            UpdateTime(Convert.ToInt32(user.Код_пользователя));
+                            LaboratoryResearcher lr = new LaboratoryResearcher();
+                            lr.Show();
+                            this.Hide();
+                            break;
+
+                        case "бухгалтер":
+                            UpdateTime(Convert.ToInt32(user.Код_пользователя));
+                            Accountant ac = new Accountant();
+                            ac.Show();
+                            this.Hide();
+                            break;
+                        
+                        case "администратор":
+                            UpdateTime(Convert.ToInt32(user.Код_пользователя));
+                            Administrator ad = new Administrator();
+                            ad.Show();
+                            this.Hide();
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Логин или пароль указаны неверно");
+                    return;
+                }
+            }
+        }
+
+        private void UpdateTime(int userKode)
+        {
+            IPAddress localIPv4 = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            using (var bd = new MedLaboratoryEntities())
+            {
+                var user = bd.Авторизация.FirstOrDefault(a=>a.Код_авторизации == userKode);
+                var history = new История();
+                userData.idUser = userKode;
+
+                user.Дата_и_время_последнего_входа = DateTime.Now;
+                user.IP_Адрес = localIPv4.ToString();
+
+                history.Код_пользователя = user.Код_пользователя;
+                history.Дата_и_время_входа = DateTime.Now;
+                history.IP_адрес = localIPv4.ToString();
+                bd.История.Add(history);
+                bd.SaveChanges();
+            }
+        }
+
+        private void passwordV_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (passwordH.Password.Length != passwordV.Text.Length)
+            {
+                passwordH.Password = passwordV.Text;
+                passwordV.Focus();
+            }
+        }
+
+        private void passwordH_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (passwordH.Password.Length != passwordV.Text.Length)
+            {
+                passwordV.Text = passwordH.Password;
+                passwordH.Focus();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (!passwordVisability)
+            {
+                passwordV.Visibility = Visibility.Visible;
+                passwordH.Visibility = Visibility.Collapsed;
+                passwordVisability = true;
+                
+            }
+            else
+            {
+                passwordV.Visibility = Visibility.Collapsed;
+                passwordH.Visibility = Visibility.Visible;
+                passwordVisability = false;
+            }
+
+        }
+    }
+}
---
+++ Day 1/MedLaboratory/MedLaboratory/LaboratoryAssistant.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static MedLaboratory.Autorisation;
+
+namespace MedLaboratory
+{
+    /// <summary>
+    /// Логика взаимодействия для LaboratoryAssistant.xaml
+    /// </summary>
+    public partial class LaboratoryAssistant : Window
+    {
+        private DispatcherTimer timer;
+        public int minutes = 30;
+        public int hour = 2;
+
+        public LaboratoryAssistant()
+        {
+            InitializeComponent();
+            Initial();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMinutes(1);
+            timer.Tick += OnTimedEvent;
+            timer.Start();
+        }
+
+        private void Initial()
+        {
+            exitTime.Content = hour + ":" + minutes;
+            using (var bd = new MedLaboratoryEntities())
+            {
+                var user = bd.Пользователи.FirstOrDefault(u=> u.Код_пользователя == userData.idUser);
+                byte[] imageData = user.Фотография;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = new MemoryStream(imageData);
+                bitmapImage.EndInit();
+                photo.Source = bitmapImage;
+
+                var role = bd.Должность.FirstOrDefault(r=>r.Код_должности == user.Код_должности);
+
+                fio.Content = user.Фамилия + " " + user.Имя + " " + role.Название;
+            }
+        }
+
+        private void OnTimedEvent(object sender, EventArgs e)
+        {
+            if (hour == 0 && minutes == 0)
+            {
+                MessageBox.Show("Время доступа закончилось");
+
+                using (var bd = new MedLaboratoryEntities())
+                {
+                    var autor = bd.Авторизация.FirstOrDefault(au => au.Код_пользователя == userData.idUser);
+
+                    autor.Время_блокировки = DateTime.Now;
+                    bd.SaveChanges();
+                }
+
+                timer.Stop();
+                Autorisation a = new Autorisation();
+                a.Show();
+                this.Close();
+            }
+
+            minutes--;
+            if (minutes == 0 && hour != 0)
+            { 
+                hour--;
+                minutes = 60;
+            }    
+
+            if(minutes > 10)
+                exitTime.Content = hour + ":" + minutes;
+            else
+                exitTime.Content = hour + ":0" + minutes;
+
+
+            if (hour == 0 && minutes == 15)
+            {
+                MessageBox.Show("До окончания сеанса осталось 15 минут");
+            }
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Autorisation a = new Autorisation();
+            a.Show();
+            this.Close();
+        }
+    }
+}
